3ebe59e47d8206ea19ad71e962abca5ef413af92
siddharth-nair-2 <nairsid2@gmail.com>
2022-07-28 12:48:14 +0530

    Form Validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -28,18 +28,21 @@ const ContactForm = () => {
             type="text"
             placeholder="Name"
             name="name"
+            required
           />
           <input
             className="my-4 p-2 bg-[#ccd6f6]"
             type="email"
             placeholder="Email"
             name="email"
+            required
           />
           <textarea
             className="bg-[#ccd6f6] p-2"
             name="message"
             rows="10"
             placeholder="Message"
+            required
           ></textarea>
           <button className="text-white border-2 hover:bg-[#E6536C] hover:border-[#E6536C] px-4 py-3 my-8 mx-auto flex items-center">
             Let's Collaborate
